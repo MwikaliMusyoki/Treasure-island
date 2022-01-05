60ae3e51e9fd7d9c4371b3e6e67dfae6976605da
Mwikali Musyoki <mwikalimusyoki.24@gmail.com>
2022-01-05 07:26:56 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+# https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+# Write your code below this line 👇
+Direction = input("In which direction would you like to start? left or right?")
+
+Sdirection = Direction.lower()
+
+if Sdirection == "left":
+    Swim_wait = input(
+        "Would you like to swim across the lake or ait for a boat ? swim or wait")
+    swimornot = Swim_wait.lower()
+    if swimornot == "wait":
+        Door = input(
+            "Congratulations on safe passage!! Which door would you like to open? Red, Yellow or Blue?")
+        door = Door.lower()
+        if door == "red":
+            print("You were burnt by the fire! GAME OVER.")
+        elif door == "blue":
+            print("You were eaten by beasts. GAME OVER.")
+        elif door == "yellow":
+            print("YOU WIN!!!!!!!!")
+        else:
+            print("GAME OVER!")
+    else:
+        print("You were attacked by crocodiles! GAME OVER.")
+else:
+    print("Ooops, you fell into a hole. GAME OVER.")
